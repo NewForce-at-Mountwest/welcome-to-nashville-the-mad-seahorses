b9f925195bc348912727a94edae1236d0b338748
david bias <bias99@mail.com>
2019-08-28 15:28:48 -0400

    meetups functions

---
+++ src/scripts/meetupsAPI.js
@@ -0,0 +1,29 @@
+// this is my private tocken EWDWCUDI2BT6IQQP74XO//
+
+
+
+const meetUpsButton = document.querySelector("#meetUpsButton")
+meetUpsButton.addEventListener("click", function () {
+    fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville_programming&token=EWDWCUDI2BT6IQQP74XO`, {
+        headers: {
+            "Authorization": `Bearer EWDWCUDI2BT6IQQP74XO`,
+            "Accept": "application/json"
+        }
+    })
+        .then(events => events.json())
+        .then(parsedevents => {
+            // console.log(parsedevents)
+        })
+    document.querySelector("meetUps-input")
+    
+    // const personName = document.querySelector("#fullName").value
+    // const personAddress = document.querySelector("#address").value
+})
+
+    // get click event working 
+
+
+
+
+
+
