8f8df8c7205f9b84be1fb6a162afb3cd22365e9b
Newforce39 <justin.warden@outlook.com>
2019-08-29 15:32:58 -0400

    finished parks with filter

--- src/scripts/parksAPI.js
+++ src/scripts/parksAPI.js
@@ -1,11 +1,32 @@
 
-fetch("http://localhost:8088/parks")
-    .then(jwparks => jwparks.json())
-    .then(parsedparks => {
 
-const jwparksbutton= document.querySelector(".parksbutton")
+const jwparksbutton= document.querySelector("#park-button")
+
+// EVENT LISTENER
 
 jwparksbutton.addEventListener("click",function(){
-document.querySelector(".results-div").innerHTML =(parsedparks)
+    const parkinput = document.querySelector(".park-input").value
+    console.log(parkinput)
+
+    // CLEAR INPUT AND SEARCH VALUE
+
+document.querySelector(".park-input").value= ""
+document.querySelector(".results-div").innerHTML = ""
+
+// GET /posts?id=1&id=2
+// http://localhost:8088/parks?dog_park=Yes
+
+// FETCH THE FILTERED RESULTS
+
+    fetch(`http://localhost:8088/parks?${parkinput}=Yes`)
+    .then(jwparks => jwparks.json())
+    .then(parsedparks => {
+        console.log(parsedparks)
+
+        for(let i=0; i<parsedparks.length; i++){
+            console.log(parsedparks.park_name)
+            document.querySelector(".results-div").innerHTML += 
+            `<p>${parsedparks[i].park_name}</p>`
+        }
+    })
 })
-        })
