4b85ab096b014a578b4b62d35d81dc6837801142
david bias <bias99@mail.com>
2019-08-30 09:48:21 -0400

    getting new stuff from homie

--- src/scripts/meetupsAPI.js
+++ src/scripts/meetupsAPI.js
@@ -1,10 +1,6 @@
 // this is my private tocken EWDWCUDI2BT6IQQP74XO//
 
-
-
-const meetUpsButton = document.querySelector("#meetUpsButton")
-meetUpsButton.addEventListener("click", function () {
-    fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville_programming&token=EWDWCUDI2BT6IQQP74XO`, {
+ fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville_programming&token=EWDWCUDI2BT6IQQP74XO`, {
         headers: {
             "Authorization": `Bearer EWDWCUDI2BT6IQQP74XO`,
             "Accept": "application/json"
@@ -12,13 +8,22 @@ meetUpsButton.addEventListener("click", function () {
     })
         .then(events => events.json())
         .then(parsedevents => {
-            // console.log(parsedevents)
-            document.querySelector(".results-div").innerHTML=(parsedevents)
-    })
-      
+            console.log( parsedevents.events)
     
-    // const personName = document.querySelector("#fullName").value
-    // const personAddress = document.querySelector("#address").value
+            const meetUpsButton = document.querySelector("#meetups-button")
+            meetUpsButton.addEventListener("click", function () {
+                // console.log("hey")
+                for(let i=0; i<parsedevents.events.length; i++){
+                    // console.log("inside the loop")
+                    document.querySelector(".results-div").innerHTML += 
+                    `<p>${parsedevents.events[i].name.html}</p>`+
+                    `<p>${parsedevents.events[i].description.html}</p>`+
+                    `<p>${parsedevents.events[i].end.html}</p>`
+                }
+                
+    })
+    
+   
 })
 
     // get click event working 
