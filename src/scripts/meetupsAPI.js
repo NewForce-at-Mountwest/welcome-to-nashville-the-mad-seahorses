1243245ad9301a7d1e420e6b828c7f79b7ab7965
david bias <bias99@mail.com>
2019-08-30 11:19:57 -0400

    pushing my new stuff up

--- src/scripts/meetupsAPI.js
+++ src/scripts/meetupsAPI.js
@@ -1,34 +1,45 @@
 // this is my private tocken EWDWCUDI2BT6IQQP74XO//
 
- fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville_programming&token=EWDWCUDI2BT6IQQP74XO`, {
-        headers: {
-            "Authorization": `Bearer EWDWCUDI2BT6IQQP74XO`,
-            "Accept": "application/json"
-        }
-    })
-        .then(events => events.json())
-        .then(parsedevents => {
-            console.log( parsedevents.events)
-    
-            const meetUpsButton = document.querySelector("#meetups-button")
-            meetUpsButton.addEventListener("click", function () {
-                // console.log("hey")
-                for(let i=0; i<parsedevents.events.length; i++){
-                    // console.log("inside the loop")
-                    document.querySelector(".results-div").innerHTML += 
-                    `<p>${parsedevents.events[i].name.html}</p>`+
-                    `<p>${parsedevents.events[i].description.html}</p>`+
-                    `<p>${parsedevents.events[i].end.html}</p>`
-                }
-                
-    })
-    
-   
+
+
+
+
+
+
+const meetUpsButton = document.querySelector("#meetups-button")
+meetUpsButton.addEventListener("click", function () {
+    // console.log("hey")
+
+    const meetupsInput = document.querySelector(".meetups-input").value;
+    console.log(meetupsInput,)
+
+
+document.querySelector(".concerts-input").value = ""
+document.querySelector(".results-div").innerHTML = ""
+
+fetch(`https://www.eventbriteapi.com/v3/events/search/?q=nashville_${meetupsInput}&token=EWDWCUDI2BT6IQQP74XO`, {
+    headers: {
+        "Authorization": `Bearer EWDWCUDI2BT6IQQP74XO`,
+        "Accept": "application/json"
+    }
 })
+    .then(events => events.json())
+    .then(parsedevents => {
+        console.log(parsedevents.events)
+
+        for (let i = 0; i < parsedevents.events.length; i++) {
+            // console.log("inside the loop")
+            document.querySelector(".results-div").innerHTML +=
+                `<p>${parsedevents.events[i].name.html}</p>`
+            // `<p>${parsedevents.events[i].description.html}</p>`
+            // `<p>${parsedevents.events[i].end.html}</p>`
+        }
+
+    })
 
     // get click event working 
 
-
+})
 
 
 
